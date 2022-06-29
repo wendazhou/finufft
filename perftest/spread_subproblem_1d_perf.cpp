8536c01a07a8774f7eb9e379db7fc75667feec39
Wenda Zhou <wz2247@nyu.edu>
2022-06-28 20:14:55 -0400

    reorder benchmark

--- perftest/spread_subproblem_1d_perf.cpp
+++ perftest/spread_subproblem_1d_perf.cpp
@@ -99,22 +99,22 @@ template <typename T, std::size_t Dim> void benchmark_reference(benchmark::State
 
 } // namespace
 
+BENCHMARK(benchmark_reference<float, 1>)
+    ->ArgsProduct({{1 << 12}, {4, 5, 6, 7, 8}})
+    ->Unit(benchmark::kMicrosecond);
 BENCHMARK(benchmark_legacy<float, 1>)
     ->ArgsProduct({{1 << 12}, {4, 5, 6, 7, 8}})
     ->Unit(benchmark::kMicrosecond);
 BENCHMARK(benchmark_direct<float, 1>)
     ->ArgsProduct({{1 << 12}, {4, 5, 6, 7, 8}})
     ->Unit(benchmark::kMicrosecond);
-BENCHMARK(benchmark_reference<float, 1>)
+
+BENCHMARK(benchmark_reference<double, 1>)
     ->ArgsProduct({{1 << 12}, {4, 5, 6, 7, 8}})
     ->Unit(benchmark::kMicrosecond);
-
 BENCHMARK(benchmark_legacy<double, 1>)
     ->ArgsProduct({{1 << 12}, {4, 5, 6, 7, 8}})
     ->Unit(benchmark::kMicrosecond);
 BENCHMARK(benchmark_direct<double, 1>)
-    ->ArgsProduct({{1 << 12}, {4, 5, 6, 7, 8}})
-    ->Unit(benchmark::kMicrosecond);
-BENCHMARK(benchmark_reference<double, 1>)
     ->ArgsProduct({{1 << 12}, {4, 5, 6, 7, 8}})
     ->Unit(benchmark::kMicrosecond);

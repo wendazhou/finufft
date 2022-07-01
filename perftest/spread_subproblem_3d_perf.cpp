2fdb55a8badaec5d76701a91277bd4250a2b4aa4
Wenda Zhou <wz2247@nyu.edu>
2022-07-01 02:40:14 -0400

    performance test for 3d subproblem

---
+++ perftest/spread_subproblem_3d_perf.cpp
@@ -0,0 +1,28 @@
+#include "spread_subproblem_perf.h"
+#include <benchmark/benchmark.h>
+
+BENCHMARK(benchmark_avx512<float, 3>)
+    ->ArgsProduct({{1 << 12}, {4, 5, 6, 7, 8}})
+    ->Unit(benchmark::kMicrosecond);
+BENCHMARK(benchmark_reference<float, 3>)
+    ->ArgsProduct({{1 << 12}, {4, 5, 6, 7, 8}})
+    ->Unit(benchmark::kMicrosecond);
+BENCHMARK(benchmark_legacy<float, 3>)
+    ->ArgsProduct({{1 << 12}, {4, 5, 6, 7, 8}})
+    ->Unit(benchmark::kMicrosecond);
+BENCHMARK(benchmark_direct<float, 3>)
+    ->ArgsProduct({{1 << 12}, {4, 5, 6, 7, 8}})
+    ->Unit(benchmark::kMicrosecond);
+
+//BENCHMARK(benchmark_avx512<double, 2>)
+//    ->ArgsProduct({{1 << 12}, {4, 5, 6, 7, 8}})
+//    ->Unit(benchmark::kMicrosecond);
+BENCHMARK(benchmark_reference<double, 3>)
+    ->ArgsProduct({{1 << 12}, {4, 5, 6, 7, 8}})
+    ->Unit(benchmark::kMicrosecond);
+BENCHMARK(benchmark_legacy<double, 3>)
+    ->ArgsProduct({{1 << 12}, {4, 5, 6, 7, 8}})
+    ->Unit(benchmark::kMicrosecond);
+BENCHMARK(benchmark_direct<double, 3>)
+    ->ArgsProduct({{1 << 12}, {4, 5, 6, 7, 8}})
+    ->Unit(benchmark::kMicrosecond);
